f358e1bbca970214e16397235d8247d4eeca430a
Alan Balcom <abalcom23@gmail.com>
2023-03-16 11:46:58 -0400

    added barChart

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+
+    return (
+        <header>
+            <img src="https://img.icons8.com/3d-fluency/94/null/money-bag-bitcoin.png" alt='Bitcoin money bag'/>
+            <h2>Arbitrage Tracker</h2>
+        </header>
+    )
+}
+
+export default Header;
