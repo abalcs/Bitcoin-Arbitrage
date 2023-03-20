404761c4c0b70ca2b045eed42c9f894c8f964622
Alan Balcom <abalcom23@gmail.com>
2023-03-19 23:32:24 -0400

    added server,  and db

---
+++ server/models/Trades.js
@@ -0,0 +1,9 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Trades extends Model {};
+
+Trades.init(
+
+);
---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+// const routes = require('./routes/api_routes');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 5050;
+
+// express returns an Object
+const app = express(); // instance = Object
+
+// Setup our server
+
+app.use('/', express.static(path.join(__dirname, 'client/build')));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// routes(app);
+
+// Heroku 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('../client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+// sync sequelize models to the database, then turn on the server
+db.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+  });
